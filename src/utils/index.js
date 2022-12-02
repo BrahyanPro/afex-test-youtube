cd9c0809de26c47c5e1d51c55ce1fe33074cb571
BrahyanPro <brahyan.martinez@enviame.io>
2022-12-02 02:25:55 -0400

    Feat created componentend and module neccesary

---
+++ src/utils/index.js
@@ -0,0 +1,5 @@
+export const apiGoogle = 'https://www.googleapis.com/youtube/v3/videos?id='
+    export const keyGoogle = '&key='+process.env.VUE_APP_YOUTUBE_KEY
+    export const apiAWS = process.env.VUE_APP_API_AWS
+    export const urlYoutube = 'https://www.youtube.com/watch?v='
+
