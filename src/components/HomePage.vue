8150e9c9fa91196d5706f10db82aaf9edf6a2ee1
BrahyanPro <brahyan.martinez@enviame.io>
2022-12-03 23:43:46 -0400

    Feat home page style

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,14 +1,23 @@
 <template>
-  <div>
-    <h1>Home Page</h1>
+  <div class="home">
     <InputAddUrl/>
+    <Gallery/>
   </div>
 </template>
 
 <script setup>
 import InputAddUrl from './InputAddUrl.vue'
+import Gallery from './Gallery.vue'
 </script>
 
 <style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
 </style>
