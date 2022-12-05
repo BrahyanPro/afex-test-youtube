10913eb242d81c11987f481dcd5b2f3750fc2ea5
BrahyanPro <brahyan.martinez@enviame.io>
2022-12-04 22:09:55 -0400

    Alert for success and delete complete

--- src/utils/alert.js
+++ src/utils/alert.js
@@ -1,26 +1,39 @@
-import { useNotification } from "@kyvg/vue3-notification";
+import Swal from 'sweetalert2'
 
-const { notify}  = useNotification()
-
-notify({
-  title: "Authorization",
-  text: "You have been logged in!",
-});
+const evaluateOption = (result, onConfirm, onClose) => {
+  if (result.isConfirmed) {
+    if (onConfirm) onConfirm()
+  } else if (
+    /* Read more about handling dismissals below */
+    result.dismiss === Swal.DismissReason.cancel
+  ) {
+    if (onClose) onClose()
+  }
+}
 
 class Alerts {
-  success(message) {
-    notify({
-      title: "Success",
-      text: message,
-      type: 'success'
-    });
+  success(message, action, title, acceptText) {
+    Swal.fire({
+      icon: 'success',
+      title: title || 'Completado',
+      html: message,
+      focusConfirm: true,
+      confirmButtonText: `${acceptText || 'Aceptar'}`,
+    }).then(result => {
+      // evaluateOption(result, action)
+      console.log(result)
+    })
   }
-  error(message) {
-    notify({
-      title: "Error",
-      text: message,
-      type: 'error'
-    });
+  error(message, action, title, acceptText) {
+    Swal.fire({
+      icon: 'error',
+      title: title || 'Alerta',
+      html: message,
+      focusConfirm: true,
+      confirmButtonText: `${acceptText || 'Aceptar'}`,
+    }).then(result => {
+      evaluateOption(result, action)
+    })
   }
   warning(message) {
     notify({
@@ -28,6 +41,20 @@ class Alerts {
       text: message,
       type: 'warning'
     });
+  }
+
+  confirm(message, action, title, acceptText, cancelText) {
+    Swal.fire({
+      icon: 'warning',
+      title: title || 'Confirmar',
+      html: message,
+      focusConfirm: true,
+      showCancelButton: true,
+      confirmButtonText: `${acceptText || 'Aceptar'}`,
+      cancelButtonText: cancelText || 'Cancelar'
+    }).then(result => {
+      evaluateOption(result, action)
+    })
   }
 }
 
